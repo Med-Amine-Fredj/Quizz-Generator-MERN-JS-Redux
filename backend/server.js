3378186a7db7f35a66b10e798cb270a5525a991b
Med-Amine-Fredj <76249803+Med-Amine-Fredj@users.noreply.github.com>
2021-06-30 02:16:15 +0100

    adding colors in console

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import  express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import colors from 'colors'
 import lesquizz from './data/lesquizz.js'
 
 
@@ -27,4 +28,4 @@ app.get('/admin/myquizz/:id',(req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode  on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode  on port ${PORT}`.yellow.bold))
